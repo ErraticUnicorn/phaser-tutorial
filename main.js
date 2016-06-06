2adf38baa0fc4688f5d2ee63c826c9eae258156b
ErraticUnicorn <david.j.wert@gmail.com>
2016-06-06 14:30:18 -0400

    adds enemies

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ var mainState = {
         game.load.image('player', 'assets/player.png');
         game.load.image('wallV', 'assets/wallVertical.png');
         game.load.image('wallH', 'assets/wallHorizontal.png');
+        game.load.image('coin', 'assets/coin.png');
+        game.load.image('enemy', 'assets/enemy.png');
     },
     create: function() {
     // This function is called after the 'preload' function
@@ -22,6 +24,23 @@ var mainState = {
         game.stage.backgroundColor = '#3498db';
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.renderer.renderSession.roundPixels = true;
+        // Display the coin
+        this.coin = game.add.sprite(60, 140, 'coin');
+        // Add Arcade physics to the coin
+        game.physics.arcade.enable(this.coin);
+        // Set the anchor point to its center
+        this.coin.anchor.setTo(0.5, 0.5);
+        // Display the score
+        this.scoreLabel = game.add.text(30, 30, 'score: 0',
+        { font: '18px Arial', fill: '#ffffff' });
+        // Initialize the score variable
+        this.score = 0;
+        
+        this.enemies = game.add.group();
+        this.enemies.enableBody = true;
+        this.enemies.createMultiple(10, 'enemy');
+        game.time.events.loop(2200, this.addEnemy, this);
+        
         this.createWorld();
     },
     update: function() {
@@ -32,6 +51,13 @@ var mainState = {
         if(!this.player.inWorld){
             this.playerDie();
         }
+        game.physics.arcade.overlap(this.player, this.coin, this.takeCoin,
+                                    null, this);
+        // Make the enemies and walls collide
+        game.physics.arcade.collide(this.enemies, this.walls);
+        // Call the 'playerDie' function when the player and an enemy overlap
+        game.physics.arcade.overlap(this.player, this.enemies, this.playerDie,
+        null, this);
     },
     
     createWorld: function() {
@@ -70,6 +96,52 @@ var mainState = {
     
     playerDie: function() {
         game.state.start('main');
+    },
+    
+    takeCoin: function(player, coin) {
+        // Increase the score by 5
+        this.score += 5;
+        // Update the score label by using its 'text' property
+        this.scoreLabel.text = 'score: ' + this.score;
+        // Change the coin position
+        this.updateCoinPosition();
+    },
+    
+    updateCoinPosition: function() {
+        // Store all the possible coin positions in an array
+        var coinPosition = [
+        {x: 140, y: 60}, {x: 360, y: 60}, // Top row
+        {x: 60, y: 140}, {x: 440, y: 140}, // Middle row
+        {x: 130, y: 300}, {x: 370, y: 300} // Bottom row
+        ];
+        // Remove the current coin position from the array
+        // Otherwise the coin could appear at the same spot twice in a row
+        for (var i = 0; i < coinPosition.length; i++) {
+        if (coinPosition[i].x == this.coin.x) {
+        coinPosition.splice(i, 1);
+        }
+        }
+        // Randomly select a position from the array with 'game.rnd.pick'
+        var newPosition = game.rnd.pick(coinPosition);
+        // Set the new position of the coin
+        this.coin.reset(newPosition.x, newPosition.y);
+    },
+    
+    addEnemy: function() {
+        // Get the first dead enemy of the group
+        var enemy = this.enemies.getFirstDead();
+        // If there isn't any dead enemy, do nothing
+        if (!enemy) {
+            return;
+        }
+        // Initialize the enemy
+        enemy.anchor.setTo(0.5, 1);
+        enemy.reset(game.width/2, 0);
+        enemy.body.gravity.y = 500;
+        enemy.body.velocity.x = 100 * game.rnd.pick([-1, 1]);
+        enemy.body.bounce.x = 1;
+        enemy.checkWorldBounds = true;
+        enemy.outOfBoundsKill = true;
     },
 };
 // We initialize Phaser
